d627cfbcf95c1419c4c2624b898e3b6b64bc8bb7
Aidan Hugo <aidanhugo@Aidans-MacBook-Pro.local>
2026-09-20 16:05:45 -0400

    Intial implementation with normalization and validation, unorganized

--- market_data_pipeline.py
+++ market_data_pipeline.py
@@ -20,3 +20,38 @@ else:
 new_date = pd.to_datetime(df["date"], errors="coerce")
 df["date"] = new_date
 inv_dates = df["date"].isna()
+
+numeric_columns = ['open','high','low','close','volume']
+for column in numeric_columns:
+    new = pd.to_numeric(df[column], errors="coerce")
+    df[column] = new
+inv_columns = df[numeric_columns].isna()
+inv_numeric_rows = inv_columns.any(axis=1)
+
+inv_open_high = df["open"] > df["high"]
+inv_open_low = df["open"] < df["low"]
+
+inv_close_high = df["close"] > df["high"]
+inv_close_low = df["close"] < df["low"]
+
+inv_prices = inv_open_high | inv_open_low | inv_close_high | inv_close_low
+
+inv_volume = df["volume"] < 0
+
+empty_symbol = df["symbol"].str.strip()
+df["symbol"] = empty_symbol
+blank_symbols = df["symbol"] == ""
+inv_symbols = blank_symbols | df["symbol"].isna()
+
+
+duplicate_columns = ['symbol', 'date']
+inv_duplicates = df[duplicate_columns].duplicated(keep=False)
+
+check = {"invalid_dates": inv_dates, "invalid_symbols": inv_symbols, "invalid_numeric": inv_numeric_rows, "invalid_prices": inv_prices, "negative_volume": inv_volume, "duplicate": inv_duplicates}
+validation_report = pd.DataFrame(data=check)
+
+inv_rows = validation_report.any(axis=1)
+rejected_rows = df[inv_rows]
+val_rows = df[~inv_rows]
+
+failures = validation_report.sum(axis=0)
