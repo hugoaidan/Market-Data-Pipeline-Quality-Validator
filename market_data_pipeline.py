d7f5d24047763cf8bba41934bcbc6cc3de4b2760
Aidan Hugo <aidanhugo@Aidans-MacBook-Pro.local>
2026-09-20 14:55:00 -0400

    Inital row checks

--- market_data_pipeline.py
+++ market_data_pipeline.py
@@ -9,4 +9,14 @@ def column_check(columns, df):
         if column not in df.columns:
             missing_col.append(column)
     return missing_col
+
+missing = column_check(columns, df)
+
+if len(missing) == 0:
+    print("Column check passed!")
+else:
+    print(f"{missing} columns are missing.")
     
+new_date = pd.to_datetime(df["date"], errors="coerce")
+df["date"] = new_date
+inv_dates = df["date"].isna()
